3feb37941419862b6faad912a77925f5d668bb8f
GabrielNkwa <gnkwa@outlook.com>
2023-07-13 16:11:11 +0100

    sanity corrections

---
+++ dist/static/index-5ccb42b3-afab7a5e.js
@@ -0,0 +1,1 @@
+import{DocumentPane as t}from"./index-26656574-5d13829c.js";import{D as m}from"./sanity-36c5a24d.js";import"./json-inspector-4fc2ca2e.js";export{m as DocumentPaneProvider,t as default};
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,10 +2,12 @@ import { visionTool } from '@sanity/vision';
 import { StudioNavbar } from './src/components/StudioNavbar';
 import { Config } from 'sanity';
 import { deskTool } from 'sanity/desk';
-import { schemaTypes } from './schemas';
+import { schemaTypes } from './schemas/index';
 import { msTheme } from './src/theme';
 
-export const config: Config = {
+import { defineConfig } from 'sanity';
+
+export default defineConfig({
   name: 'default',
   title: 'Web Store',
   basePath: '/studio',
@@ -14,11 +16,9 @@ export const config: Config = {
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
-
   schema: {
     types: schemaTypes,
   },
-
   theme: msTheme,
 
   studio: {
@@ -26,4 +26,27 @@ export const config: Config = {
       navbar: StudioNavbar,
     },
   },
-};
+});
+
+// export const config: Config = {
+//   name: 'default',
+//   title: 'Web Store',
+//   basePath: '/studio',
+
+//   projectId: 'djq50trl',
+//   dataset: 'production',
+
+//   plugins: [deskTool(), visionTool()],
+
+//   schema: {
+//     types: schemaTypes,
+//   },
+
+//   theme: msTheme,
+
+//   studio: {
+//     components: {
+//       navbar: StudioNavbar,
+//     },
+//   },
+// };
